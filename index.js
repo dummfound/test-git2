ed32b91165f242ffefd7924570b046ff104e05a9
Andrey Chesnokov <dummfound@gmail.com>
2022-06-21 13:07:27 +0300

    add gitignore

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const str = 'My name is Andrey';
 const newStr = str.toLocaleLowerCase().slice(0,1) + str.toLocaleUpperCase().slice(1, str.length);
 console.log(newStr)
+console.log(newStr)
+console.log(newStr)
